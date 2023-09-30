a7b23dcb59c4d99455a5fc454656cf3432a7c50f
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-10-01 00:23:22 +0500

    base setting: animation, colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,42 @@ const twColors = require('tailwindcss/colors')
 
 const colors = {
 	transparent: twColors.transparent,
-	black: twColors.black,
-	white: twColors.white
+	black: '#2e3239',
+	white: twColors.white,
+	secondary: '#FF9902',
+	primary: '#161D25',
+	'bg-color': '#F2F2F65',
+	aqua: '#268697'
 }
 
 module.exports = {
 	content: ['./app/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {}
+		extend: {
+			keyframes: {
+				animationOpacity: {
+					from: { opacity: 0.2 },
+					to: { opacity: 1 }
+				},
+				scaleIn: {
+					'0%': {
+						opacity: 0,
+						transform: 'scale(0.9)'
+					},
+					'50%': {
+						opacity: 0.4
+					},
+					'100%': {
+						opacity: 1,
+						transform: 'scale(1)'
+					}
+				}
+			},
+			animation: {
+				opacity: 'animationOpacity .5s ease-in-out',
+				scaleIn: 'scaleIn .35 ease-in-out'
+			}
+		}
 	},
 	plugins: []
 }
