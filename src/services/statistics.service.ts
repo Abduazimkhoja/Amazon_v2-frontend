a815a4d4d05a8012e45a96c3f8896f0a5463b90b
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-10-05 22:32:30 +0500

    stats service

---
+++ src/services/statistics.service.ts
@@ -0,0 +1,17 @@
+import { instance } from '@/api/api.interceptor'
+
+const STATISTICS = 'statistics'
+
+export type TypeStatsResponse = {
+	name: string
+	value: number
+}[]
+
+export const StatisticService = {
+	async getMain() {
+		return instance<TypeStatsResponse[]>({
+			url: `${STATISTICS}/main`,
+			method: 'GET'
+		})
+	}
+}
