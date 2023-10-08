e1906c62e37d3eee45e430cfe85e38818785bf57
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-10-08 22:39:59 +0500

    redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,33 @@
-import "@/assets/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/assets/styles/globals.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import { persistor, store } from '../src/store/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import AuthProvider from '@/providers/auth-provider/AuthProvider'
+import { TypeComponentAuthFields } from '@/providers/auth-provider/auth-page.types'
 
-export default function App({ Component, pageProps }: AppProps) {
-   return <Component {...pageProps} />;
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false
+		}
+	}
+})
+
+export default function App({
+	Component,
+	pageProps
+}: AppProps & TypeComponentAuthFields) {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+					<AuthProvider Component={{ isOnlyUser: Component.isOnlyUser }}>
+						<Component {...pageProps} />
+					</AuthProvider>
+				</PersistGate>
+			</Provider>
+		</QueryClientProvider>
+	)
 }
