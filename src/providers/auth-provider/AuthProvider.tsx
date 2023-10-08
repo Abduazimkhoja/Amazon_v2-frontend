c192f4986a894487eef061f233d875ffc7526cf8
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-10-08 22:41:56 +0500

    Auth providers

---
+++ src/providers/auth-provider/AuthProvider.tsx
@@ -0,0 +1,38 @@
+import { useAuth } from '@/hooks/useAction'
+import { useActions } from '@/hooks/useActions'
+import { getAccessToken } from '@/services/auth/auth.helper'
+import dynamic from 'next/dynamic'
+import { useRouter } from 'next/router'
+import { FC, PropsWithChildren, useEffect } from 'react'
+import { TypeComponentAuthFields } from './auth-page.types'
+import Cookies from 'js-cookie'
+
+const DynamicCheckRole = dynamic(() => import('./CheckRole'), { ssr: false })
+
+const AuthProvider: FC<PropsWithChildren<TypeComponentAuthFields>> = ({
+	Component: { isOnlyUser },
+	children
+}) => {
+	const { user } = useAuth(),
+		{ checkAuth, logout } = useActions(),
+		{ pathname } = useRouter()
+
+	useEffect(() => {
+		const accessToken = getAccessToken()
+		if (accessToken) checkAuth()
+	}, [])
+
+	useEffect(() => {
+		const refreshToken = Cookies.get('refreshToken')
+
+		if (refreshToken && user) logout()
+	}, [pathname])
+
+	return isOnlyUser ? (
+		<DynamicCheckRole Component={{ isOnlyUser }}>{children}</DynamicCheckRole>
+	) : (
+		<>{children}</>
+	)
+}
+
+export default AuthProvider
