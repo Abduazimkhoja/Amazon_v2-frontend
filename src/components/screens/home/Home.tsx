168eddfd0902cdc7f836780528dac4da31664b3a
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-10-18 23:57:58 +0500

    Catalog TypeProducts

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,8 +1,14 @@
+import { TypeProducts } from '@/types/product.interface'
 import Meta from '@/ui/Meta'
+import Catalog from '@/ui/catalog/catalog'
 import { FC } from 'react'
 
-const Home: FC = () => {
-	return <Meta title='Home'>Home</Meta>
+const Home: FC<TypeProducts> = ({ products }) => {
+	return (
+		<Meta title='Home'>
+			<Catalog products={products} />
+		</Meta>
+	)
 }
 
 export default Home
