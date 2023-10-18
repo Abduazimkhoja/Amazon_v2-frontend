b4bba32dee0d1e0b54f16c8d2875999b1fb05af6
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-10-18 23:57:16 +0500

    getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
 import Home from '@/screens/home/Home'
-import { NextPage } from 'next'
+import { ProductService } from '@/services/product/product.service'
+import { TypeProducts } from '@/types/product.interface'
+import { GetStaticProps, NextPage } from 'next'
 
-const HomePage: NextPage = () => {
-	return <Home />
+const HomePage: NextPage<TypeProducts> = ({ products }) => {
+	return <Home products={products}/>
+}
+
+export const getStaticProps: GetStaticProps<TypeProducts> = async () => {
+	const { data: products } = await ProductService.getAll({
+		page: 1,
+		perPage: 4
+	})
+
+	return { props: { products } }
 }
 
 export default HomePage
