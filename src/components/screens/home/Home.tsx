bbe5b06011c3ee248df3e29cf15f03f9f61ee1e9
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-10-19 23:54:33 +0500

    TypeProducts-ЮTypePaginationProducts, attr length

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,12 +1,12 @@
-import { TypeProducts } from '@/types/product.interface'
+import { TypePaginationProducts } from '@/types/product.interface'
 import Meta from '@/ui/Meta'
 import Catalog from '@/ui/catalog/catalog'
 import { FC } from 'react'
 
-const Home: FC<TypeProducts> = ({ products }) => {
+const Home: FC<TypePaginationProducts> = ({ products, length }) => {
 	return (
 		<Meta title='Home'>
-			<Catalog products={products} />
+			<Catalog products={products || []} />
 		</Meta>
 	)
 }
