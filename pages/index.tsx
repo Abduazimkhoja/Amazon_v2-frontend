7231978c12ed8b257fd04269d4932cbc27580d8a
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-10-19 23:58:35 +0500

    TypeProducts->TypePaginationProducts and attr length

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,21 @@
 import Home from '@/screens/home/Home'
 import { ProductService } from '@/services/product/product.service'
-import { TypeProducts } from '@/types/product.interface'
+import { TypePaginationProducts } from '@/types/product.interface'
 import { GetStaticProps, NextPage } from 'next'
 
-const HomePage: NextPage<TypeProducts> = ({ products }) => {
-	return <Home products={products}/>
+const HomePage: NextPage<TypePaginationProducts> = ({ length, products }) => {
+	return <Home products={products} length={length} />
 }
 
-export const getStaticProps: GetStaticProps<TypeProducts> = async () => {
-	const { data: products } = await ProductService.getAll({
+export const getStaticProps: GetStaticProps<
+	TypePaginationProducts
+> = async () => {
+	const { data } = await ProductService.getAll({
 		page: 1,
 		perPage: 4
 	})
 
-	return { props: { products } }
+	return { props: data }
 }
 
 export default HomePage
