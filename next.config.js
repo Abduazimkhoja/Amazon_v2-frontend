054706cd48c6e31e61a0343d3a386d5b7d662ed7
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-10-19 23:43:02 +0500

    env APP_URL and images domains

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
 	reactStrictMode: true,
 	env: {
 		SERVER_URL: process.env.SERVER_URL,
-	}
+		APP_URL: process.env.APP_URL
+	},
+	images: {domains: ['ae01.alicdn.com', 'picsum.photos']}
 }
 
 module.exports = nextConfig
