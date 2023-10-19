d27124a660c7b6ffdaae1368968192e1779d48e0
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-10-19 23:57:29 +0500

    errorChecking add optional chining and remove From Storage fixed the check

--- src/api/api.interceptor.ts
+++ src/api/api.interceptor.ts
@@ -1,7 +1,7 @@
-import axios from 'axios'
-import { errorCatch, getContentType } from './api.helper'
 import { getAccessToken, removeFromStorage } from '@/services/auth/auth.helper'
 import AuthService from '@/services/auth/auth.service'
+import axios from 'axios'
+import { errorCatch, getContentType } from './api.helper'
 
 export const instance = axios.create({
 	baseURL: process.env.SERVER_URL,
@@ -21,14 +21,12 @@ instance.interceptors.response.use(
 	config => config,
 	async error => {
 		const originalRequest = error.config
-
 		let errorChecking =
-			(error.response.status === 401 ||
+			(error?.response?.status === 401 ||
 				errorCatch(error) === 'jwt expired' ||
 				errorCatch(error) === 'jwt must be provided') &&
 			error.config &&
 			!error.config._isRetry
-
 		if (errorChecking) {
 			originalRequest._isRetry = true
 			try {
@@ -36,9 +34,7 @@ instance.interceptors.response.use(
 				await AuthService.getNewTokens()
 				return instance.request(originalRequest)
 			} catch (error) {
-				if (errorCatch(error) === 'jwt expired') {
-				}
-				removeFromStorage()
+				if (errorCatch(error) === 'jwt expired') removeFromStorage()
 			}
 		}
 
