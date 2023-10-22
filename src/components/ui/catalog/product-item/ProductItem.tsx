8d88bf0c911c53fe5cb533adb2f85d38e0391200
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-10-22 22:53:34 +0500

    add site navigate Link, and styles

--- src/components/ui/catalog/product-item/ProductItem.tsx
+++ src/components/ui/catalog/product-item/ProductItem.tsx
@@ -1,5 +1,7 @@
 import { IProduct } from '@/types/product.interface'
+import { convertPrice } from '@/utils/convertPrice'
 import Image from 'next/image'
+import Link from 'next/link'
 import { FC } from 'react'
 import AddToCartButton from './AddToCartButton'
 import FavoriteButton from './FavoriteButton'
@@ -8,21 +10,32 @@ import ProductRating from './ProductRating'
 const ProductItem: FC<{ product: IProduct }> = ({ product }) => {
 	return (
 		<div>
-			<div>
-				<FavoriteButton productId={product.id} />
-				<AddToCartButton product={product} />
+			<div className='bg-white rounded-xl relative overflow-hidden'>
+				<div className='absolute top-2 right-3 z-10'>
+					<FavoriteButton productId={product.id} />
+					<AddToCartButton product={product} />
+				</div>
 
-				<Image
-					width={300}
-					height={300}
-					src={product.images[0]}
-					alt={product.name}
-				/>
+				<Link href={`/product/${product.slug}`}>
+					<Image
+						width={250}
+						height={250}
+						src={product.images[0]}
+						alt={product.name}
+					/>
+				</Link>
 			</div>
-			<h3>{product.name}</h3>
-			<h5>{product.category.name}</h5>
+			<Link href={`/product/${product.slug}`}>
+				<h3 className='mt-2 font-semibold'>{product.name}</h3>
+			</Link>
+			<Link
+				href={`/category/${product.category.slug}`}
+				className='text-aqua text-xs mb-2'
+			>
+				{product.category.name}
+			</Link>
 			<ProductRating product={product} />
-			<h4>{product.price}</h4>
+			<h4 className='text-2xl font-semibold'>{convertPrice(product.price)}</h4>
 		</div>
 	)
 }
