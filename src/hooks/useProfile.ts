0b7afa2088938e7d6a90774f4fd3eb0616a4a82a
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-10-22 22:50:58 +0500

    error handle

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,10 +1,15 @@
 import { UserService } from '@/services/user.service'
-import { IFullUser } from '@/types/user.interface'
 import { useQuery } from '@tanstack/react-query'
+import { useAuth } from './useAction'
 
 export const useProfile = () => {
+	const { user } = useAuth()
 	const { data } = useQuery(['get profile'], () => UserService.getProfile(), {
-		select: ({ data }) => data
+		select: ({ data }) => data,
+		onError: error => {
+			console.log(error)
+		},
+		enabled: !!user
 	})
 
 	return { profile: data }
