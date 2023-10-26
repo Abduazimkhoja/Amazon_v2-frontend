3e2bb3a8c4dd5d2d97bf798823273f9ce375a376
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-10-26 23:27:04 +0500

    add Layout

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,12 +1,29 @@
+import { useAuth } from '@/hooks/useAction'
+import { useActions } from '@/hooks/useActions'
 import { TypePaginationProducts } from '@/types/product.interface'
 import Meta from '@/ui/Meta'
-import Catalog from '@/ui/catalog/catalog'
+import Catalog from '@/ui/catalog/Catalog'
+import Layout from '@/ui/layout/Layout'
+import { useRouter } from 'next/router'
 import { FC } from 'react'
 
 const Home: FC<TypePaginationProducts> = ({ products, length }) => {
+	const { user } = useAuth()
+	const { logout } = useActions()
+	const router = useRouter()
+
 	return (
 		<Meta title='Home'>
-			<Catalog title='Fresh products' products={products || []} />
+			<Layout>
+				{!!user && <button onClick={() => logout()}>Logout</button>}
+				{!user && <button onClick={() => router.push('/auth')}>Sign In</button>}
+
+				<Catalog
+					title='Fresh products'
+					products={products || []}
+					isPagination
+				/>
+			</Layout>
 		</Meta>
 	)
 }
