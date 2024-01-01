3aa01696f27a1452a91ea3fdf603aefd65b32f3f
abduazimxoja <abdusalomov.aa03@gmail.com>
2024-01-01 23:01:26 +0500

    refactor: add interface

--- src/components/ui/catalog/SortDropdown.tsx
+++ src/components/ui/catalog/SortDropdown.tsx
@@ -1,14 +1,25 @@
 import { EnumProductSort } from '@/services/product/product.types'
-import { FC } from 'react'
+import { Dispatch, FC, SetStateAction } from 'react'
 
-const SortDropdown: FC = () => {
+interface ISortDropdown {
+	sortType: EnumProductSort
+	setSortType: Dispatch<SetStateAction<EnumProductSort>>
+}
+
+const SortDropdown: FC<ISortDropdown> = ({ sortType, setSortType }) => {
 	return (
 		<div className='text-right mb-5'>
-			<select className='appearance-none py-1 px-2 bg-white'>
+			<select
+				value={sortType}
+				onChange={e => setSortType(e.target.value as any)}
+				className='appearance-none py-1 px-2 bg-white'
+			>
 				{(
 					Object.keys(EnumProductSort) as Array<keyof typeof EnumProductSort>
 				).map(key => (
-					<option value={EnumProductSort[key]}>{EnumProductSort[key]}</option>
+					<option key={key} value={EnumProductSort[key]}>
+						{EnumProductSort[key]}
+					</option>
 				))}
 			</select>
 		</div>
@@ -16,4 +27,3 @@ const SortDropdown: FC = () => {
 }
 
 export default SortDropdown
- 
