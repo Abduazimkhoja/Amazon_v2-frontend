f71934c77adfcf217e3375f20755d6241efb0df2
abduazimxoja <abdusalomov.aa03@gmail.com>
2024-01-03 19:31:20 +0500

    feat: add cart item

---
+++ src/components/ui/layout/cart/cart-item/CartItem.jsx
@@ -0,0 +1,16 @@
+const CartItem: FC<{item: ICartItem}> = ({item}) => {
+   return (
+      <div className={styles.item}>
+         <Image src={item.product.images[0]} width={100} height={100} alt={item.product.name}/>
+         <div>
+            <div className={styles.name}>{item.product.name}</div>
+            <div className={styles.price}>
+               {convertPrice(item.product.price)}
+            </div>
+            <CartActions item={item}/>
+         </div>
+      </div>
+   )
+}
+
+export default CartItem
