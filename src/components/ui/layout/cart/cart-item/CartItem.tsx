411d8b8a00d281ba0d4d9982f62c72a8542943cf
abduazimxoja <abdusalomov.aa03@gmail.com>
2024-01-06 19:20:05 +0500

    refactor: rename file

---
+++ src/components/ui/layout/cart/cart-item/CartItem.tsx
@@ -0,0 +1,24 @@
+import { ICartItem } from '@/types/cart.interface'
+import { convertPrice } from '@/utils/convertPrice'
+import { FC } from 'react'
+import CartActions from './cart-action/cartAction'
+
+const CartItem: FC<{ item: ICartItem }> = ({ item }) => {
+	return (
+		<div className={styles.item}>
+			<Image
+				src={item.product.images[0]}
+				width={100}
+				height={100}
+				alt={item.product.name}
+			/>
+			<div>
+				<div className={styles.name}>{item.product.name}</div>
+				<div className={styles.price}>{convertPrice(item.product.price)}</div>
+				<CartActions item={item} />
+			</div>
+		</div>
+	)
+}
+
+export default CartItem
