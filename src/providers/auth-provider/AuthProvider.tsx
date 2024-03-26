d622c32167e9e2fc79965ee687dc7eada8de8b8a
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-03-26 20:23:52 +0500

    refactor: added page display based on the user's role

--- src/providers/auth-provider/AuthProvider.tsx
+++ src/providers/auth-provider/AuthProvider.tsx
@@ -1,21 +1,19 @@
+'use client'
+import NotFound from '@/app/not-found'
+import { ADMIN_PANEL_URL } from '@/config/url.config'
+import { REFRESH_TOKEN } from '@/constants/token.constants'
 import { useAuth } from '@/hooks/useAction'
 import { useActions } from '@/hooks/useActions'
 import { getAccessToken } from '@/services/auth/auth.helper'
 import Cookies from 'js-cookie'
-import dynamic from 'next/dynamic'
-import { useRouter } from 'next/router'
+import { usePathname, useRouter } from 'next/navigation'
 import { FC, PropsWithChildren, useEffect } from 'react'
-import { TypeComponentAuthFields } from './auth-page.types'
+import { protectedRoutes } from './protected-routes.data'
 
-const DynamicCheckRole = dynamic(() => import('./CheckRole'), { ssr: false })
-
-const AuthProvider: FC<PropsWithChildren<TypeComponentAuthFields>> = ({
-	Component: { isOnlyUser },
-	children
-}) => {
-	const { user } = useAuth(),
-		{ checkAuth, logout } = useActions(),
-		{ pathname } = useRouter()
+const AuthProvider: FC<PropsWithChildren<unknown>> = ({ children }) => {
+	const { user } = useAuth()
+	const { checkAuth, logout } = useActions()
+	const pathname = usePathname()
 
 	useEffect(() => {
 		const accessToken = getAccessToken()
@@ -23,16 +21,43 @@ const AuthProvider: FC<PropsWithChildren<TypeComponentAuthFields>> = ({
 	}, [])
 
 	useEffect(() => {
-		const refreshToken = Cookies.get('refreshToken')
+		const refreshToken = Cookies.get(REFRESH_TOKEN)
 
 		if (!refreshToken && user) logout()
 	}, [pathname])
 
-	return isOnlyUser ? (
-		<DynamicCheckRole Component={{ isOnlyUser }}>{children}</DynamicCheckRole>
-	) : (
-		<>{children}</>
+	const router = useRouter()
+
+	const isProtectedRoute = protectedRoutes.some(route =>
+		pathname?.startsWith(route)
 	)
+	const isAdminRoute = pathname?.startsWith(ADMIN_PANEL_URL)
+
+	// если пользователь не авторизован и заходит на скрытую страницу то перенаправляется на регистрацию
+	if (isProtectedRoute && !user && pathname !== '/auth') {
+		router.replace('/auth')
+		return null
+	}
+	// если пользователь не адним то такой страницы не существует
+	if (!user?.isAdmin && isAdminRoute) return <NotFound />
+
+  // скрытие страницы регистрации для авторизированых пользователей
+	if (user && pathname === '/auth') {
+		router.replace('/')
+		return null
+	}
+
+	return <>{children}</>
+	// if (!isProtectedRoute && !isAdminRoute) return <>{children}</>
+
+	// if (user?.isAdmin) return <>{children}</>
+
+	// if (user && isProtectedRoute) return <>{children}</>
+
+	// if (user && isAdminRoute) return <NotFound />
+
+	// pathname !== '/auth' && router.replace('/auth')
+	// return null
 }
 
 export default AuthProvider
