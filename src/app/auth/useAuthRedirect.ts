7e759c7d87f73f44cda2eaa708cb3260e4620bfa
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-03-26 20:16:34 +0500

    feat: add auth page

--- src/app/auth/useAuthRedirect.ts
+++ src/app/auth/useAuthRedirect.ts
@@ -1,5 +1,5 @@
 import { useAuth } from '@/hooks/useAction'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 
 export const useAuthREdirect = () => {
