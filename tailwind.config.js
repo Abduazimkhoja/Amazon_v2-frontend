4488ddafb2ad1f204d2ab32c2862f98e21e90a87
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-03-31 20:49:59 +0100

    refactor: add z-index and fontSize

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ const colors = {
 	primary: '#FF9902',
 	'bg-color': '#F2F2F6',
 	aqua: '#268697',
-	red: twColors.red[400],
+	red: twColors.red[400]
 }
 
 module.exports = {
@@ -19,6 +19,27 @@ module.exports = {
 	theme: {
 		colors,
 		extend: {
+			zIndex: {
+				1: 1,
+				2: 2,
+				3: 3
+			},
+			fontSize: {
+				xs: '0.82rem',
+				sm: '0.98rem',
+				base: '1.15rem',
+				lg: '1.22rem',
+				xl: '1.36rem',
+				'1.5xl': '1.5rem',
+				'2x1': '1.725rem',
+				'3x1': '2.155rem',
+				'4x1': '2.58rem',
+				'5x1': '3.45rem',
+				'6x1': '4.3rem',
+				'7x1': '5.17rem',
+				'8x1': '6.9rem',
+				'9x1': '9.2rem'
+			},
 			keyframes: {
 				animationOpacity: {
 					from: { opacity: 0.2 },
