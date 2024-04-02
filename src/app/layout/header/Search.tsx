9b427b4e0d136cc0b2960c3b396d62bbfc5a0cb9
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-02 19:21:46 +0000

    refactor: changed push url

--- src/app/layout/header/Search.tsx
+++ src/app/layout/header/Search.tsx
@@ -10,24 +10,29 @@ const Search: FC = () => {
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault()
 		if (searchTerm.trim() !== '') {
-			router.push(`/q?term=${encodeURIComponent(searchTerm)}`)
+			router.push(`/explorer?searchTerm=${encodeURIComponent(searchTerm)}`)
 		}
 	}
 
 	return (
 		<form onSubmit={handleSubmit}>
 			<div
-				className='border border-solid border-gray/10 grid 1/2 rounded-xl overflow-hidden'
+				className='border border-solid border-gray/10 grid w-1/3 rounded-xl overflow-hidden'
 				style={{ gridTemplateColumns: '1fr 0.1fr' }}
 			>
 				<input
 					type='search'
 					className='bg-[#22303e] text-sm py-2 px-4 text-white'
-          value={searchTerm}
-          onChange={e=> setSearchTerm(e.target.value)}
-          placeholder='Search...'
+					value={searchTerm}
+					onChange={e => setSearchTerm(e.target.value)}
+					placeholder='Search...'
 				/>
-        <button type='submit' className='bg-primary text-white flex items-center justify-center p-2.5'><BsSearch/></button>
+				<button
+					type='submit'
+					className='bg-primary text-white flex items-center justify-center p-2.5'
+				>
+					<BsSearch />
+				</button>
 			</div>
 		</form>
 	)
