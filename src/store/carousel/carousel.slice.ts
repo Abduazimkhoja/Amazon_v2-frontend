a777e1dc01d4927b040445ed9fa230be4fb3198b
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-03 00:27:34 +0500

    feat: create carousel slice

---
+++ src/store/carousel/carousel.slice.ts
@@ -0,0 +1,25 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { ICarouselInitialState } from './carusel.types'
+
+const initialState: ICarouselInitialState = {
+	selectedItemIndex: 0
+}
+
+export const carouselSlice = createSlice({
+	name: 'carousel',
+	initialState,
+	reducers: {
+		nextSlide: (state, action: PayloadAction<{ carouselLength: number }>) => {
+			if (state.selectedItemIndex !== action.payload.carouselLength - 1)
+				state.selectedItemIndex += 1
+			else state.selectedItemIndex = 0
+		},
+		prevSlide: (state, action: PayloadAction<{ carouselLength: number }>) => {
+			if (state.selectedItemIndex > 0) state.selectedItemIndex -= 1
+			else state.selectedItemIndex = action.payload.carouselLength - 1
+		},
+		selectSlide: (state, action: PayloadAction<number>) => {
+			state.selectedItemIndex = action.payload
+		}
+	}
+})
