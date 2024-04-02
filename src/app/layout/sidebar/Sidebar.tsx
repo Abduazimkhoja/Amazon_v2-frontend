8373d5fc220ea3c72590ee962ade7cf2d27e0583
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-03 00:29:00 +0500

    feat: add admin panel sidebar

--- src/app/layout/sidebar/Sidebar.tsx
+++ src/app/layout/sidebar/Sidebar.tsx
@@ -1,67 +1,56 @@
 'use client'
+import { useCategories } from '@/hooks/queries/useCategories'
 import { useAuth } from '@/hooks/useAction'
 import { useActions } from '@/hooks/useActions'
+import { useIsAdminPanel } from '@/hooks/useIsAdminPanel'
 import { CategoryService } from '@/services/category.service'
 import Loader from '@/ui/Loader'
 import { useQuery } from '@tanstack/react-query'
 import cn from 'clsx'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import { FC } from 'react'
 import { FiLogOut } from 'react-icons/fi'
+import { ADMIN_MENU } from './admin-menu.data'
+import { convertToMenuItems } from './convert-to-menu-items'
 
 const Sidebar: FC = () => {
-	const { data, isLoading } = useQuery({
-		queryKey: ['get categories'],
-		queryFn: () => CategoryService.getAll(),
-		select: ({ data }) => data
-	})
-	const pathname = usePathname()
+	const { data, isLoading } = useCategories()
 
-	const { user } = useAuth()
-	const { logout } = useActions()
+	const { isAdminPanel, pathname } = useIsAdminPanel()
 
 	return (
 		<aside
-			className='flex flex-col justify-between bg-secondary'
-			style={{ height: 'calc(100vh - 91px)' }}
+			className='flex flex-col justify-between bg-secondary z-10'
+			style={{ height: 'calc(100vh - 91px)', minHeight: `calc(100vh - 91px)` }}
 		>
 			<div>
 				{isLoading ? (
 					<Loader />
 				) : data ? (
 					<>
-						<h4 className='text-xl text-white mt-4 mb-6 ml-6'>Categories:</h4>
+						<h4 className='text-xl text-white mt-4 mb-6 ml-6'>
+							{isAdminPanel ? 'Menu' : 'Categories'}
+						</h4>
 						<ul>
-							{data.map(category => (
-								<li key={category.id}>
-									<Link
-										className={cn(
-											'block text-lg my-3 px-10 hover:text-primary transition-colors duration-200',
-											pathname === `/category/${category.slug}`
-												? 'text-primary'
-												: 'text-white'
-										)}
-										href={`/category/${category.slug}`}
-									>
-										{category.name}
-									</Link>
-								</li>
-							))}
+							{(isAdminPanel ? ADMIN_MENU : convertToMenuItems(data)).map(
+								item => (
+									<li key={item.href}>
+										<Link
+											className={cn(
+												'block text-lg my-3 px-10 hover:text-primary transition-colors duration-200',
+												pathname === item.href ? 'text-primary' : 'text-white'
+											)}
+											href={item.href}
+										>
+											{item.label}
+										</Link>
+									</li>
+								)
+							)}
 						</ul>
 					</>
 				) : (
 					<div>Categories not found!</div>
-				)}
-
-				{!!user && (
-					<button
-						className='text-white flex items-center ml-10 mb-10'
-						onClick={() => logout()}
-					>
-						<FiLogOut />
-						<span className='ml-2'>Logout</span>
-					</button>
 				)}
 			</div>
 		</aside>
