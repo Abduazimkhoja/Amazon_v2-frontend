74f1094307824a17b8ce11f0b34d09dac7d9b0f0
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-03 00:26:20 +0500

    feat: add get by use id

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -14,16 +14,22 @@ enum EnumOrderStatus {
 type TypeData = {
 	status?: EnumOrderStatus
 	items: {
-    quantity: number
-    price: number
-    productId: number
-  }[]
+		quantity: number
+		price: number
+		productId: number
+	}[]
 }
 
 export const OrderService = {
 	async getAll() {
 		return instance<IOrder[]>({
 			url: ORDERS,
+			method: 'GET'
+		})
+	},
+	async getByUserId() {
+		return instance<IOrder[]>({
+			url: `${ORDERS}/by-user`,
 			method: 'GET'
 		})
 	},
