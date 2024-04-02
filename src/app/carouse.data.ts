70d415d9c1ccce7b3e616eb2019431d96ca1b783
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-02 19:21:57 +0000

    feat: add carousel data

---
+++ src/app/carouse.data.ts
@@ -0,0 +1,15 @@
+import { ICarouselItem } from '@/ui/carousel/carouse.interface'
+
+export const carouselItems: ICarouselItem[] = [
+	{
+		title: 'Free Delivery',
+		description:
+			"Don't miss it out! 0nly today, get free Next Day delivery on all of your orders.",
+		image: '/images/carousel/technology.png'
+	},
+	{
+		title: 'New products',
+		description: 'New products are already waiting for you on our shelves',
+		image: '/images/carousel/dots.png'
+	}
+]
