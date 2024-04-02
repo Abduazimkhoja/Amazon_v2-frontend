02013dbd8be5954a50940a6840deda4ac925f037
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-02 19:22:41 +0000

    refactor: import admin link, user profile

--- src/app/layout/header/Header.tsx
+++ src/app/layout/header/Header.tsx
@@ -1,37 +1,24 @@
-import { useAuth } from '@/hooks/useAction'
-import SquareButton from '@/ui/button/SquareButton'
 import Logo from '@/ui/logo/Logo'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
 import { FC } from 'react'
 import { AiOutlineHeart } from 'react-icons/ai'
-import { MdOutlineLogin } from 'react-icons/md'
+import AdminLink from './AdminLink'
 import HeaderProfile from './HeaderProfile'
 import Search from './Search'
 import HeaderCart from './cart/HeaderCart'
 
 const Header: FC = () => {
-	const router = useRouter()
-	const { user } = useAuth()
-	// const user = useRouter()
-
 	return (
 		<header className='bg-secondary w-full px-5 py-1 grid grid-cols-[auto,1fr,auto] items-center gap-4 '>
 			<Logo />
 			<Search />
 			<div className='flex items-center justify-end gap-10'>
+				<AdminLink />
 				<Link href='/favorites' className='text-white'>
 					<AiOutlineHeart size={28} />
 				</Link>
 				<HeaderCart />
-				{user ? (
-					<HeaderProfile />
-				) : (
-					<SquareButton
-						Icon={MdOutlineLogin}
-						onClick={() => router.push('/auth')}
-					/>
-				)}
+				<HeaderProfile />
 			</div>
 		</header>
 	)
