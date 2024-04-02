e3288bd1d1638494c3df0c0ae0e6245c823985d6
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-02 19:18:57 +0000

    feat: add header admin link

---
+++ src/app/layout/header/AdminLink.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { useAuth } from '@/hooks/useAction'
+import { useIsAdminPanel } from '@/hooks/useIsAdminPanel'
+import Link from 'next/link'
+import { FC } from 'react'
+import { MdOutlineAdminPanelSettings } from 'react-icons/md'
+
+const AdminLink: FC = () => {
+	const { isAdminPanel } = useIsAdminPanel()
+	const { user } = useAuth()
+	const isAdminAndNotAdminPanel = user?.isAdmin && !isAdminPanel
+
+	return (
+		<div>
+			{isAdminAndNotAdminPanel && (
+				<Link
+					href='/admin'
+					className='hover:text-primary transition-colors duration-200 text-white inline-block text-lg'
+				>
+					<MdOutlineAdminPanelSettings size={29} />
+				</Link>
+			)}
+		</div>
+	)
+}
+
+export default AdminLink
