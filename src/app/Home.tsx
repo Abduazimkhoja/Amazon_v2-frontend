df3bde220e42c9062ba9f4a2ac8829bce4d65d83
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-04 05:53:45 +0500

    feat: use carouse

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,10 +1,13 @@
 import { TypePaginationProducts } from '@/types/product.interface'
+import Carousel from '@/ui/carousel/Carousel'
 import Catalog from '@/ui/catalog/Catalog'
 import { FC } from 'react'
+import { carouselItems } from './carouse.data'
 
 const Home: FC<TypePaginationProducts> = ({ products }) => {
 	return (
 		<>
+			<Carousel items={carouselItems} className='mb-10' />
 			<Catalog title='Fresh products' products={products} />
 		</>
 	)
