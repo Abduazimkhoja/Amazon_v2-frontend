d7d82de6ae0f64b24a5ebb8784d134d007772e10
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-06 13:28:04 +0500

    feat: add select component

--- src/components/ui/select/Select.tsx
+++ src/components/ui/select/Select.tsx
@@ -1,29 +1,41 @@
-import { FC } from 'react'
-import styles from './select.module.scss'
+import cn from 'clsx'
+import { useState } from 'react'
+import { BsCaretDownFill } from 'react-icons/bs'
+import styles from './Select.module.scss'
+import { ISelect } from './select.interface'
 
-const Select: FC = () => {
+function Select<K>({ data, onChange, value, title }: ISelect<K>) {
+	const [isOpen, setIsOpen] = useState(false)
+
 	return (
-		<div className='relative'>
-			<button className='flex items-center h-8 pl-3 pr-2 border border-black focus:outline-none'>
-				<span className='text-sm leading-none'>Dropdown</span>
-				<svg
-					className='w-4 h-4 mt-px ml-2'
-					xmlns='http://www.w3.org/2000/svg'
-					viewBox='0 0 20 20'
-					fill='currentColor'
-				>
-					<path
-						fill-rule='evenodd'
-						d='M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z'
-						clip-rule='evenodd'
-					/>
-				</svg>
+		<div className={styles.select}>
+			<button onClick={() => setIsOpen(!isOpen)}>
+				{title && <b>{title}</b>}
+				{value?.label || 'Default'}
+				<BsCaretDownFill />
 			</button>
-			<div className='absolute flex flex-col w-40 mt-1 z-20 bg-white border border-black shadow-lg'>
-				<div className={styles.option}>
-					<p>Item 1</p>
-				</div>
-			</div>
+			{isOpen && (
+				<ul>
+					{data.map(item => (
+						<li
+							key={item.key?.toString()}
+							className={cn({
+								[styles.active]: item.key === value?.key
+							})}
+						>
+							<button
+								onClick={() => {
+									onChange(item)
+									setIsOpen(false)
+								}}
+								disabled={item.key === value?.key}
+							>
+								{item.label}
+							</button>
+						</li>
+					))}
+				</ul>
+			)}
 		</div>
 	)
 }
