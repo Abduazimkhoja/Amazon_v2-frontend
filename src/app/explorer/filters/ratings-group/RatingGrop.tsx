80baaa59c6b339636b607f0ed072f5e7d27e635a
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-08 13:15:30 +0500

    feat: add rating group

---
+++ src/app/explorer/filters/ratings-group/RatingGrop.tsx
@@ -0,0 +1,44 @@
+import Checkbox, { ICheckbox } from '@/ui/checkbox/Checkbox'
+import { FC } from 'react'
+import { Rating } from 'react-simple-star-rating'
+import { useFilters } from '../../useFilters'
+import FilterWrapper from '../FilterWrapper'
+import { RATING_VARIANTS } from './ratings-variants.data'
+import { updateRatingsQuery } from './update-ratings-query'
+
+const RatingGroup: FC = () => {
+	const { queryParams, updateQueryParams } = useFilters()
+
+	const checkboxProps = (rating: number) => {
+		const ratingStr = rating.toString()
+		return {
+			key: rating,
+			className: 'mb-2 text-sm',
+			isChecked: queryParams.ratings?.includes(ratingStr),
+			onClick: () =>
+				updateQueryParams(
+					'ratings',
+					updateRatingsQuery(queryParams.ratings, ratingStr)
+				)
+		} as ICheckbox
+	}
+
+	const ratingProps = {
+		readonly: true,
+		SVGstyle: { display: 'inline-block' },
+		size: 20,
+		transition: true
+	}
+
+	return (
+		<FilterWrapper title='Number of reviews'>
+			{RATING_VARIANTS.map(rating => (
+				<Checkbox {...checkboxProps(rating)}>
+					<Rating initialValue={rating} {...ratingProps} />
+				</Checkbox>
+			))}
+		</FilterWrapper>
+	)
+}
+
+export default RatingGroup
