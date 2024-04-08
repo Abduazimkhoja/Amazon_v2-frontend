f064f4f46bd4dd054bafe9e334d8d5b2adb4f2de
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-08 13:09:10 +0500

    feat: add price group

---
+++ src/app/explorer/filters/price-group/PriceGroup.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react'
+import { useFilters } from '../../useFilters'
+import Range, { IRange } from '@/ui/range/Range'
+import FilterWrapper from '../FilterWrapper'
+
+const PriceGroup: FC = () => {
+	const { queryParams, updateQueryParams } = useFilters()
+
+	const rangeProps: IRange = {
+		max: 200,
+		fromInitialValue: queryParams.minPrice,
+		toInitialValue: queryParams.maxPrice,
+		onChangeFromValue: value => updateQueryParams('minPrice', value),
+		onChangeToValue: value => updateQueryParams('maxPrice', value)
+	}
+
+	return (
+		<FilterWrapper title='Price from/to'>
+			<Range {...rangeProps} />
+		</FilterWrapper>
+	)
+}
+
+export default PriceGroup
