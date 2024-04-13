76b4769bb20afa19be05e18481c286c60e5ff7cc
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-13 15:37:49 +0500

    feat: complete admin dashboards

--- src/app/admin/Dashboard.tsx
+++ src/app/admin/Dashboard.tsx
@@ -1,7 +1,11 @@
 'use client'
 import { StatisticService } from '@/services/statistics.service'
+import Heading from '@/ui/Heading'
+import Loader from '@/ui/Loader'
+import { convertPrice } from '@/utils/convertPrice'
 import { useQuery } from '@tanstack/react-query'
 import { FC } from 'react'
+import styles from './Dashboard.module.scss'
 
 const Dashboard: FC = () => {
 	const { data, isFetching } = useQuery({
@@ -10,7 +14,29 @@ const Dashboard: FC = () => {
 		select: ({ data }) => data
 	})
 
-	return <div>Dashboard</div>
+	return (
+		<>
+			<Heading className='mb-8'>Dashboard</Heading>
+			{isFetching ? (
+				<Loader />
+			) : data?.length ? (
+				<div className={styles.wrapper}>
+					{data.map((item, index) => (
+						<div key={item.name} className={styles.item}>
+							<div>{item.name}</div>
+							<div>
+								{index === data.length - 1
+									? convertPrice(item.value || 0)
+									: item.value}
+							</div>
+						</div>
+					))}
+				</div>
+			) : (
+				<div>Statistics not loaded!</div>
+			)}
+		</>
+	)
 }
 
 export default Dashboard
