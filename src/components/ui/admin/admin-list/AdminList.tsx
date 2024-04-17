a3554a043d2a99d6df430d2d34841b60b89740c0
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-17 20:44:58 +0000

    feat: add admin list

--- src/components/ui/admin/admin-list/AdminList.tsx
+++ src/components/ui/admin/admin-list/AdminList.tsx
@@ -1,8 +1,40 @@
 'use client'
+import Loader from '@/ui/Loader'
 import { FC } from 'react'
+import styles from './AdminList.module.scss'
+import AdminListItem from './AdminListItem'
+import { IListItem } from './admin-list.interface'
 
-const AdminList: FC = () => {
-	return <div>Admin list</div>
+interface IAdminList {
+	listItems?: IListItem[]
+	isLoading: boolean
+	removeHandler?: (id: number) => void
+}
+
+const AdminList: FC<IAdminList> = ({
+	isLoading,
+	removeHandler,
+	listItems = []
+}) => {
+	return (
+		<div>
+			{isLoading ? (
+				<Loader />
+			) : listItems.length ? (
+				listItems.map(listItem => (
+					<AdminListItem
+						key={listItem.id}
+						removeHandler={
+							removeHandler ? () => removeHandler(listItem.id) : undefined
+						}
+						listItem={listItem}
+					/>
+				))
+			) : (
+				<div className={styles.notFound}>Elements not found</div>
+			)}
+		</div>
+	)
 }
 
 export default AdminList
