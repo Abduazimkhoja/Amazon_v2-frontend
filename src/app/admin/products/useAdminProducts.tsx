4acd62cd0d93fd3de0320ccd16f8b105774050d5
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-18 22:47:44 +0500

    feat: use admin complete

--- src/app/admin/products/useAdminProducts.tsx
+++ src/app/admin/products/useAdminProducts.tsx
@@ -1,9 +1,39 @@
+import { getAdminUrl } from '@/config/url.config'
 import { ProductService } from '@/services/product/product.service'
-import { useQuery } from '@tanstack/react-query'
+import { IListItem } from '@/ui/admin/admin-list/admin-list.interface'
+import { formatDate } from '@/utils/fromat-date'
+import { useMutation, useQuery } from '@tanstack/react-query'
 
 export const useAdminProducts = () => {
 	const { data, isFetching, refetch } = useQuery({
 		queryKey: ['get admin product'],
-		queryFn: () => ProductService.getAll()
+		queryFn: () => ProductService.getAll(),
+		select: data =>
+			data.products.map((product): IListItem => {
+				return {
+					id: product.id,
+					viewUrl: `/product/${product.slug}`,
+					editUrl: getAdminUrl(`/products/edit/${product.id}`),
+					items: [
+						product.name,
+						product.category.name,
+						formatDate(product.createdAt)
+					]
+				}
+			})
 	})
+
+	const { mutate } = useMutation({
+		mutationKey: ['delete product'],
+		mutationFn: (id: number) => ProductService.delete(id),
+		onSuccess() {
+			refetch()
+		}
+	})
+
+	return {
+		mutate,
+		data,
+		isFetching
+	}
 }
