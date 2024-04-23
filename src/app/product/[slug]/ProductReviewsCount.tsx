43b3f3821b683ca35c04db90da841b004ecf2d60
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-23 16:24:27 +0500

    feat add reviews count,

--- src/app/product/[slug]/ProductReviewsCount.tsx
+++ src/app/product/[slug]/ProductReviewsCount.tsx
@@ -1,7 +1,33 @@
+import { IProduct } from '@/types/product.interface'
+import ProductRating from '@/ui/catalog/product-item/ProductRating'
 import { FC } from 'react'
+import { FiChevronRight } from 'react-icons/fi'
+import { Link } from 'react-scroll'
 
-const ProductReviewsCount: FC = () => {
-    return <div>ProductReviewsCount</div>
+interface IProductReviews {
+	product: IProduct
+}
+
+const ProductReviewsCount: FC<IProductReviews> = ({ product }) => {
+	const reviewsLength = product.reviews.length
+	if (!reviewsLength) return null
+
+	return (
+		<div>
+			<ProductRating product={product} />
+			<div>
+				<Link
+					className='opacity-50 font-semibold text-sm cursor-pointer'
+					to='reviews'
+					smooth
+					offset={-50}
+					duration={1000}
+				>
+					{reviewsLength} Reviews <FiChevronRight className='inline' />
+				</Link>
+			</div>
+		</div>
+	)
 }
 
 export default ProductReviewsCount
