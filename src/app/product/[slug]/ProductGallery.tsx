0c0cd40d6f208641e18b452424b01e53e4694884
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-23 16:42:01 +0500

    feat: add product gallery component

--- src/app/product/[slug]/ProductGallery.tsx
+++ src/app/product/[slug]/ProductGallery.tsx
@@ -1,7 +1,56 @@
-import { FC } from 'react'
+import Image from 'next/image'
+import { FC, useState } from 'react'
+import cn from 'clsx'
 
-const ProductGallery: FC = () => {
-    return <div>ProductGallery</div>
+interface IProductGallery {
+	images: string[]
+}
+
+const ProductGallery: FC<IProductGallery> = ({ images }) => {
+	const [activeIndex, setActiveIndex] = useState(0)
+
+	return (
+		<div>
+			<Image
+				src={images[activeIndex]}
+				alt=''
+				width={500}
+				height={500}
+				className='rounded-lg overflow-hidden'
+				priority
+				draggable={false}
+			/>
+			<div
+				className='bt-6'
+				style={{ width: '500px', overflowX: 'auto', whiteSpace: 'nowrap' }}
+			>
+				{images.map((image, index) => {
+					return (
+						<button
+							key={index}
+							onClick={() => setActiveIndex(index)}
+							className={cn(
+								'duration-300 hover:shadow-md mr-5 last:mr-0 border-b-2 border-solid transition-all rounded-lg overflow-hidden inline-block',
+								{
+									'shadow-md bg-primary': index === activeIndex,
+									'border-transparent': index !== activeIndex
+								}
+							)}
+						>
+							<Image
+								draggable={false}
+								src={image}
+								alt=''
+								width={100}
+								height={100}
+								priority
+							/>
+						</button>
+					)
+				})}
+			</div>
+		</div>
+	)
 }
 
 export default ProductGallery
