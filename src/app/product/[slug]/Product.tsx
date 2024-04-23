ac02b79f88210058098947da10328783585be113
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-23 19:12:20 +0500

    feat: import ProductReviewsCount, ProductGallery, ProductInformation

--- src/app/product/[slug]/Product.tsx
+++ src/app/product/[slug]/Product.tsx
@@ -4,6 +4,9 @@ import { IProduct } from '@/types/product.interface'
 import Heading from '@/ui/Heading'
 import { useQuery } from '@tanstack/react-query'
 import { FC } from 'react'
+import ProductGallery from './ProductGallery'
+import ProductReviewsCount from './ProductReviewsCount'
+import ProductInformation from './product-information/ProductInformation'
 
 interface IProductPage {
 	initialProduct: IProduct
@@ -26,8 +29,21 @@ const Product: FC<IProductPage> = ({
 	return (
 		<>
 			<Heading className='mb-1'>{product.name}</Heading>
+			<ProductReviewsCount product={product} />
+			<div
+				className='grid gap-12 mt-6'
+				style={{ gridTemplateColumns: '1fr 1fr 1fr' }}
+			>
+				<ProductGallery images={product.images} />
+				<div className='opacity-80 font-light'>
+					<div className='font-semibold mb-1'>Description:</div>
+					{product.description}
+				</div>
+				<ProductInformation product={product} />
+			</div>
 		</>
 	)
 }
 
 export default Product
+
