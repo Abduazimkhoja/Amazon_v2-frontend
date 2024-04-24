ee4705e687cd43bd054aedc945a9eee42bd76081
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-24 18:53:23 +0500

    feat:  add modal component

---
+++ src/components/ui/modal/Modal.tsx
@@ -0,0 +1,35 @@
+import type { FC, PropsWithChildren } from 'react'
+import { useRef } from 'react'
+import styles from './Modal.module.scss'
+import { createPortal } from 'react-dom'
+import { RiCloseFill } from 'react-icons/ri'
+
+interface IModal {
+	isOpen: boolean
+	closeModal: () => void
+}
+
+const Modal: FC<PropsWithChildren<IModal>> = ({
+	children,
+	closeModal,
+	isOpen
+}) => {
+	const modalRef = useRef<HTMLElement | null>(document.getElementById('modal'))
+
+	if (!isOpen || !modalRef.current) {
+		return null
+	}
+
+	return createPortal(
+		<div className={styles.overlay}>
+			<div className={styles.window}>
+				<button onClick={closeModal}>
+					<RiCloseFill />
+				</button>
+			</div>
+		</div>,
+		modalRef.current
+	)
+}
+
+export default Modal
