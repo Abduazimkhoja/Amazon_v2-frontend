03e410caf2e726c72a5d494fe359076c829b7bd8
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-24 18:54:04 +0500

    feat: add product reviews

--- src/app/product/[slug]/product-reviews.tsx/ProductReviews.tsx
+++ src/app/product/[slug]/product-reviews.tsx/ProductReviews.tsx
@@ -1,7 +1,42 @@
-import { FC } from 'react'
+import { useAuth } from '@/hooks/useAction'
+import { IReview } from '@/types/review.interface'
+import Heading from '@/ui/Heading'
+import Modal from '@/ui/modal/Modal'
+import { FC, useState } from 'react'
+import LeaveReviewForm from './LeaveReviewForm'
+import ReviewItem from './ReviewItem'
 
-const ProductReviews: FC = () => {
-    return <div>ProductReviews</div>
+interface IProductReviews {
+	reviews: IReview[]
+	productId: number
+}
+
+const ProductReviews: FC<IProductReviews> = ({ reviews, productId }) => {
+	const [isModalOpen, setIsModalOpen] = useState(false)
+	const { user } = useAuth()
+
+	if (!reviews.length) return null
+
+	return (
+		<section>
+			<div className='mb-9'>
+				<Heading className='mb-3'>Reviews: </Heading>
+				{user && <button className='text-aqua'>Leave a review</button>}
+			</div>
+
+			{user && (
+				<Modal isOpen={isModalOpen} closeModal={() => setIsModalOpen(false)}>
+					<LeaveReviewForm productId={productId} />
+				</Modal>
+			)}
+
+			<div className='grid grid-cols-4 gap-10'>
+				{reviews.map(review => (
+					<ReviewItem key={review.id} review={review} />
+				))}
+			</div>
+		</section>
+	)
 }
 
 export default ProductReviews
