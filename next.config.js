f4bc6332e9648df2d548059b61b7935b9e484389
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-25 19:30:32 +0500

    fix: rewrites

--- next.config.js
+++ next.config.js
@@ -24,7 +24,7 @@ const nextConfig = {
 		return [
 			{
 				source: `/uploads/:path*`,
-				destination: `${SERVER_URL}/uploads/:path*`
+				destination: `${process.env.SERVER_URL}/uploads/:path*`
 			}
 		]
 	}
