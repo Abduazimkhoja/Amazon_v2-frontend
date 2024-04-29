61fb3a6b16d1f2c41d4c7f9ab66addb5a73a40e4
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-29 22:40:33 +0500

    refactor: remove drop-down menu

--- src/app/layout/header/cart/HeaderCart.tsx
+++ src/app/layout/header/cart/HeaderCart.tsx
@@ -1,58 +1,57 @@
 'use client'
 import { useCart } from '@/hooks/useCart'
-import { useOutside } from '@/hooks/useOutside'
 import SquareButton from '@/ui/button/SquareButton'
-import { convertPrice } from '@/utils/convertPrice'
+
 import Link from 'next/link'
 import type { FC } from 'react'
 import { RiShoppingCartLine } from 'react-icons/ri'
-import CartItem from './cart-item/CartItem'
-import styles from './cart.module.scss'
 
 const Cart: FC = () => {
-	const { isShow, setIsShow, ref } = useOutside(false)
+	// const { isShow, setIsShow, ref } = useOutside(false)
 	const { items, total } = useCart()
 
 	return (
-		<div className='relative' ref={ref}>
+		// <div className='relative' ref={ref}>
+		<Link href='/checkout' className='relative'>
 			<SquareButton
 				Icon={RiShoppingCartLine}
-				onClick={() => {
-					setIsShow(!isShow)
-				}}
+				// onClick={() => {
+				// 	setIsShow(!isShow)
+				// }}
 				number={items.length}
 			/>
-			{isShow && (
-				<div className={styles.cartWrapper}>
-					<div className='font-normal text-lg mb-5'>My cart</div>
+		</Link>
+		// 	{isShow && (
+		// 		<div className={styles.cartWrapper}>
+		// 			<div className='font-normal text-lg mb-5'>My cart</div>
 
-					<div className={styles.cart}>
-						{items.length ? (
-							items.map(item => <CartItem item={item} key={item.id} />)
-						) : (
-							<div className='font-light'>Cart is empty!</div>
-						)}
-					</div>
+		// 			<div className={styles.cart}>
+		// 				{items.length ? (
+		// 					cartList.map(item => <CartItem item={item} key={item.id} />)
+		// 				) : (
+		// 					<div className='font-light'>Cart is empty!</div>
+		// 				)}
+		// 			</div>
 
-					<div className={styles.footer}>
-						<div>Total:</div>
-						<div>{convertPrice(total)}</div>
-					</div>
+		// 			<div className={styles.footer}>
+		// 				<div>Total:</div>
+		// 				<div>{convertPrice(total)}</div>
+		// 			</div>
 
-					{!!items.length && (
-						<div className='text-center mt-7 mb5'>
-							<Link
-								className='btn btn-white'
-								href='/checkout'
-								onClick={() => setIsShow(false)}
-							>
-								Go to checkout
-							</Link>
-						</div>
-					)}
-				</div>
-			)}
-		</div>
+		// 			{!!items.length && (
+		// 				<div className='text-center mt-7 mb5'>
+		// 					<Link
+		// 						className='btn btn-white'
+		// 						href='/checkout'
+		// 						onClick={() => setIsShow(false)}
+		// 					>
+		// 						Go to checkout
+		// 					</Link>
+		// 				</div>
+		// 			)}
+		// 		</div>
+		// 	)}
+		// </div>
 	)
 }
 
