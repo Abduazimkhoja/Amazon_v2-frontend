2f25bf6d463a0b13d035abee45493edd06959d79
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-30 21:50:18 +0500

    refactor: redesigned on chakra ui and removed the drop-down

--- src/app/layout/header/cart/HeaderCart.tsx
+++ src/app/layout/header/cart/HeaderCart.tsx
@@ -7,51 +7,12 @@ import type { FC } from 'react'
 import { RiShoppingCartLine } from 'react-icons/ri'
 
 const Cart: FC = () => {
-	// const { isShow, setIsShow, ref } = useOutside(false)
-	const { items, total } = useCart()
+	const { items } = useCart()
 
 	return (
-		// <div className='relative' ref={ref}>
 		<Link href='/checkout' className='relative'>
-			<SquareButton
-				Icon={RiShoppingCartLine}
-				// onClick={() => {
-				// 	setIsShow(!isShow)
-				// }}
-				number={items.length}
-			/>
+			<SquareButton Icon={RiShoppingCartLine} number={items.length} />
 		</Link>
-		// 	{isShow && (
-		// 		<div className={styles.cartWrapper}>
-		// 			<div className='font-normal text-lg mb-5'>My cart</div>
-
-		// 			<div className={styles.cart}>
-		// 				{items.length ? (
-		// 					cartList.map(item => <CartItem item={item} key={item.id} />)
-		// 				) : (
-		// 					<div className='font-light'>Cart is empty!</div>
-		// 				)}
-		// 			</div>
-
-		// 			<div className={styles.footer}>
-		// 				<div>Total:</div>
-		// 				<div>{convertPrice(total)}</div>
-		// 			</div>
-
-		// 			{!!items.length && (
-		// 				<div className='text-center mt-7 mb5'>
-		// 					<Link
-		// 						className='btn btn-white'
-		// 						href='/checkout'
-		// 						onClick={() => setIsShow(false)}
-		// 					>
-		// 						Go to checkout
-		// 					</Link>
-		// 				</div>
-		// 			)}
-		// 		</div>
-		// 	)}
-		// </div>
 	)
 }
 
