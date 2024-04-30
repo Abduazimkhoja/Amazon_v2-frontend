6b11f40bed224bbbb5a77397a43cdc50de92377d
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-04-30 21:41:48 +0500

    refactor: update convertImageUrl

--- src/utils/convert-image-url.ts
+++ src/utils/convert-image-url.ts
@@ -1,3 +1,7 @@
-export const convertImageUrl = (images: string[]) => {
-  return `${process.env.SERVER_URL}${images[0]}`
+export const convertImageUrl = (
+	image: string[] | string,
+	index: number = 0
+) => {
+	if (Array.isArray(image)) return `${process.env.SERVER_URL}${image[index]}`
+	else return `${process.env.SERVER_URL}${image}`
 }
