2785bab8fbed285ffaab9e4baa65555e788f7857
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-05-01 14:41:24 +0500

    refactor: sort dropdown rewrote the chakra ui

--- src/app/explorer/sort/SortDropdown.tsx
+++ src/app/explorer/sort/SortDropdown.tsx
@@ -1,22 +1,40 @@
 'use client'
 
-import { EnumProductSort } from '@/services/product/product.types'
-import Select from '@/ui/select/Select'
-import type { FC } from 'react'
+// import Select from '@/ui/select/Select'
+import { Select } from '@chakra-ui/react'
+import type { ChangeEvent, FC } from 'react'
 import { useFilters } from '../useFilters'
 import { SORT_SELECT_DATA } from './sort-select.data'
 
 const SortDropdown: FC = () => {
 	const { queryParams, updateQueryParams } = useFilters()
 
+	const selectChange = (event: ChangeEvent<HTMLSelectElement>) => {
+		const select = event.target.value
+
+		updateQueryParams('sort', select)
+	}
+
+	const selectDefailtValue =
+		SORT_SELECT_DATA.find(value => value.key === queryParams.sort)?.key ||
+		SORT_SELECT_DATA[0].key
+
 	return (
 		<div className='text-right z-10'>
-			<Select<EnumProductSort>
-				data={SORT_SELECT_DATA}
-				onChange={value => updateQueryParams('sort', value.key.toString())}
-				value={SORT_SELECT_DATA.find(value => value.key === queryParams.sort)}
+			<Select
+				zIndex='1'
+				onChange={event => selectChange(event)}
+				value={selectDefailtValue}
 				title='Sort by'
-			/>
+			>
+				{SORT_SELECT_DATA.map(item => {
+					return (
+						<option key={item.key} value={item.key}>
+							{item.label}
+						</option>
+					)
+				})}
+			</Select>
 		</div>
 	)
 }
