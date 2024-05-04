abb561986a5ab1ef14d4a116e62374c0a0dbb1bf
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-05-04 23:35:55 +0500

    refactor: add toltip in favorite button

--- src/components/ui/catalog/product-item/FavoriteButton.tsx
+++ src/components/ui/catalog/product-item/FavoriteButton.tsx
@@ -1,14 +1,16 @@
 'use client'
 import { useProfile } from '@/hooks/useProfile'
 import { UserService } from '@/services/user.service'
-import { IconButton } from '@chakra-ui/react'
+import { IconButton, Tooltip } from '@chakra-ui/react'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useRouter } from 'next/navigation'
 import { type FC } from 'react'
 import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
 
 const FavoriteButton: FC<{ productId: number }> = ({ productId }) => {
 	const { profile } = useProfile()
 	const queryClient = useQueryClient()
+	const { push } = useRouter()
 
 	const { mutate } = useMutation({
 		mutationKey: ['toggle favorite'],
@@ -18,21 +20,27 @@ const FavoriteButton: FC<{ productId: number }> = ({ productId }) => {
 		}
 	})
 
-	if (!profile) return null
+	const isExists = profile?.favorites.some(
+		favorite => favorite.id === productId
+	)
 
-	const isExists = profile.favorites.some(favorite => favorite.id === productId)
-
-	const icon = isExists ? <AiFillHeart /> : <AiOutlineHeart />
+	const icon = isExists ? (
+		<AiFillHeart className='text-red' />
+	) : (
+		<AiOutlineHeart className='text-red' />
+	)
 
 	return (
-		<IconButton
-			bg='transparent'
-			onClick={() => {
-				mutate()
-			}}
-			aria-label='Search database'
-			icon={icon}
-		/>
+		<Tooltip label='Add to favorite'>
+			<IconButton
+				bg='transparent'
+				onClick={() => {
+					!!profile ? mutate() : push('/auth')
+				}}
+				aria-label='Search database'
+				icon={icon}
+			/>
+		</Tooltip>
 	)
 }
 
