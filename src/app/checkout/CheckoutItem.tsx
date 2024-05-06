1b167e3d0f10d0b9b49cd7a8941b1a93145addcd
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-05-06 23:52:46 +0500

    refactor: add chakra ui

--- src/app/checkout/CheckoutItem.tsx
+++ src/app/checkout/CheckoutItem.tsx
@@ -1,30 +1,108 @@
 'use client'
+import { useActions } from '@/hooks/useActions'
 import { IProduct, IProducts } from '@/types/product.interface'
+import FavoriteButton from '@/ui/catalog/product-item/FavoriteButton'
 import { convertImageUrl } from '@/utils/convert-image-url'
 import { convertPrice } from '@/utils/convertPrice'
+import {
+	Box,
+	Center,
+	Divider,
+	Flex,
+	Heading,
+	IconButton,
+	Text,
+	Tooltip,
+	Wrap
+} from '@chakra-ui/react'
 import Image from 'next/image'
+import Link from 'next/link'
 import type { FC } from 'react'
-import styles from './Checkout.module.scss'
+import { BiTrash } from 'react-icons/bi'
+import CartActions from '../layout/header/cart/cart-item/cart-action/cartAction'
 
-const CheckoutItem: FC<{ product: IProduct | IProducts }> = ({ product }) => {
-	const { images, name, category, price } = product
+const CheckoutItem: FC<{ product: IProduct | IProducts; itemId: number }> = ({
+	itemId,
+	product
+}) => {
+	const { id, images, name, category, price, slug } = product
+	const { removeFromCart } = useActions()
 
 	return (
-		<div className={styles.item}>
-			<Image
-				src={convertImageUrl(images)}
-				width={100}
-				height={100}
-				alt={name}
-			/>
-			<div className={styles.row}>
-				<div className={styles.information}>
-					<div>{name}</div>
-					<div>{category.slug}</div>
-				</div>
-				<div className={styles.price}>{convertPrice(price)}</div>
-			</div>
-		</div>
+		<Flex border='1px' borderColor='blackAlpha.100'>
+			<Wrap minW='140px' maxW='180px'>
+				<Image
+					width={180}
+					height={200}
+					className='w-full h-auto object-cover'
+					src={convertImageUrl(images)}
+					alt={name}
+				/>
+			</Wrap>
+			<Flex flexDirection='column' justifyContent='space-between' flex='1'>
+				<Center
+					flex='1'
+					gap='3'
+					paddingInline='7'
+					paddingBlock='4'
+					justifyContent='space-between'
+				>
+					<Box paddingBlock='2'>
+						<Link className='flex-1' href={`/product/${slug}`}>
+							<Heading
+								mb='2'
+								fontWeight='semibold'
+								lineHeight='tight'
+								size='md'
+								noOfLines={1}
+							>
+								<Tooltip label={name} aria-label='A tooltip'>
+									{name}
+								</Tooltip>
+							</Heading>
+						</Link>
+						<Link href={`/category/${category.slug}`}>
+							<Text
+								color='gray.500'
+								fontWeight='semibold'
+								letterSpacing='wide'
+								fontSize='xs'
+								textTransform='uppercase'
+							>
+								<Tooltip label={category.slug} aria-label='A tooltip'>
+									{category.slug}
+								</Tooltip>
+							</Text>
+						</Link>
+					</Box>
+					<Flex gap='2'>
+						<FavoriteButton productId={id} />
+						<Tooltip label='Remove product' aria-label='A tooltip'>
+							<IconButton
+								aria-label='remove cart item'
+								icon={<BiTrash />}
+								onClick={() => removeFromCart({ id: itemId })}
+							/>
+						</Tooltip>
+					</Flex>
+				</Center>
+
+				<Divider />
+
+				<Center
+					gap='3'
+					paddingInline='7'
+					paddingBlock='4'
+					justifyContent='space-between'
+				>
+					<CartActions itemId={itemId} />
+
+					<Text paddingBlock='2' fontWeight='600' fontSize='2xl'>
+						{convertPrice(price)}
+					</Text>
+				</Center>
+			</Flex>
+		</Flex>
 	)
 }
 
