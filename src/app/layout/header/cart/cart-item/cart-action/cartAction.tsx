28616b4a16077484a9018ded570be3167821b1d8
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-05-06 23:52:25 +0500

    refactor: add chakra ui

--- src/app/layout/header/cart/cart-item/cart-action/cartAction.tsx
+++ src/app/layout/header/cart/cart-item/cart-action/cartAction.tsx
@@ -1,43 +1,47 @@
 'use client'
 import { useActions } from '@/hooks/useActions'
 import { useCart } from '@/hooks/useCart'
-import { ICartItem } from '@/types/cart.interface'
-import type { FC } from 'react'
-import { FiMinus, FiPlus, FiTrash } from 'react-icons/fi'
+import { Button, HStack, Input, useNumberInput } from '@chakra-ui/react'
+import { useEffect, type FC } from 'react'
+import { FiMinus, FiPlus } from 'react-icons/fi'
 
-const CartActions: FC<{ item: ICartItem }> = ({ item }) => {
-	const { removeFromCart, changeQuantity } = useActions()
+const CartActions: FC<{ itemId: number }> = ({ itemId }) => {
+	const { changeQuantity } = useActions()
 	const { items } = useCart()
 
-	const quantity = items.find(cartItem => cartItem.id === item.id)?.quantity
+	const quantity = items.find(cartItem => cartItem.id === itemId)?.quantity || 1
+
+	const {
+		valueAsNumber,
+		getInputProps,
+		getIncrementButtonProps,
+		getDecrementButtonProps
+	} = useNumberInput({
+		defaultValue: quantity,
+		min: 1,
+		max: 10
+	})
+
+	const inc = getIncrementButtonProps()
+	const dec = getDecrementButtonProps()
+	const input = getInputProps()
+
+	useEffect(() => {
+		if (isNaN(valueAsNumber)) return undefined
+		changeQuantity({
+			id: itemId,
+			type: 'assign',
+			value: valueAsNumber
+		})
+	}, [valueAsNumber])
 
 	return (
-		<div className='mt-3'>
-			<div className='flex items-center gap-3'>
-				<button onClick={() => changeQuantity({ id: item.id, type: 'minus' })}>
-					<FiMinus fontSize={13} />
-				</button>
-
-				<input
-					disabled
-					readOnly
-					value={quantity}
-					className='w-10 bg-black text-center'
-					type='text'
-				/>
-
-				<button onClick={() => changeQuantity({ id: item.id, type: 'plus' })}>
-					<FiPlus fontSize={13} />
-				</button>
-
-				<button
-					onClick={() => removeFromCart({ id: item.id })}
-					className='ml-3 text-dark-primary'
-				>
-					<FiTrash />
-				</button>
-			</div>
-		</div>
+		<HStack maxW='320px'>
+			<Button {...dec}>-</Button>
+			{quantity}
+			<Input w='16' textAlign='center' {...input} />
+			<Button {...inc}>+</Button>
+		</HStack>
 	)
 }
 
