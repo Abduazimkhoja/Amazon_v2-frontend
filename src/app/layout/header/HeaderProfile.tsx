0a4ec8356dc26985e4c52745634c6571eb2e09d6
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-05-14 19:54:36 +0000

    refactor: add modal to confirm logout

--- src/app/layout/header/HeaderProfile.tsx
+++ src/app/layout/header/HeaderProfile.tsx
@@ -3,7 +3,14 @@ import { useAuth } from '@/hooks/useAction'
 import { useActions } from '@/hooks/useActions'
 import { useProfile } from '@/hooks/useProfile'
 import {
+	AlertDialog,
+	AlertDialogBody,
+	AlertDialogContent,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogOverlay,
 	Avatar,
+	Button,
 	IconButton,
 	Menu,
 	MenuButton,
@@ -11,11 +18,13 @@ import {
 	MenuGroup,
 	MenuItem,
 	MenuList,
-	Tooltip
+	Tooltip,
+	useDisclosure
 } from '@chakra-ui/react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import type { FC } from 'react'
+import React from 'react'
 import { MdOutlineLogin } from 'react-icons/md'
 
 const HeaderProfile: FC = () => {
@@ -23,6 +32,8 @@ const HeaderProfile: FC = () => {
 	const { user } = useAuth()
 	const router = useRouter()
 	const { logout } = useActions()
+	const { isOpen, onOpen, onClose } = useDisclosure()
+	const cancelRef = React.useRef<HTMLButtonElement | null>(null)
 
 	if (!user) {
 		return (
@@ -39,30 +50,58 @@ const HeaderProfile: FC = () => {
 	}
 
 	return (
-		<Menu>
-			<MenuButton>
-				{profile?.avatarPath && (
-					<Avatar
-						border='2px solid orange'
-						size='md'
-						name='Ryan Florence'
-						src={profile.avatarPath}
-					/>
-				)}
-			</MenuButton>
-			<MenuList zIndex='20'>
-				<MenuGroup title='Profile'>
-					<MenuItem>My Account</MenuItem>
-					<MenuItem>
-						<Link href='/my-orders'>Payments</Link>
-					</MenuItem>
-					<MenuDivider />
-					<MenuItem borderTop='ActiveBorder' onClick={() => logout()}>
-						Logout
-					</MenuItem>
-				</MenuGroup>
-			</MenuList>
-		</Menu>
+		<>
+			<Menu>
+				<MenuButton>
+					{profile?.avatarPath && (
+						<Avatar
+							border='2px solid orange'
+							size='md'
+							name='Ryan Florence'
+							src={profile.avatarPath}
+						/>
+					)}
+				</MenuButton>
+				<MenuList zIndex='20'>
+					<MenuGroup title='Profile'>
+						<MenuItem>My Account</MenuItem>
+						<MenuItem>
+							<Link href='/my-orders'>Payments</Link>
+						</MenuItem>
+						<MenuDivider />
+						<MenuItem borderTop='ActiveBorder' onClick={onOpen}>
+							Logout
+						</MenuItem>
+					</MenuGroup>
+				</MenuList>
+			</Menu>
+			<AlertDialog
+				isOpen={isOpen}
+				leastDestructiveRef={cancelRef}
+				onClose={onClose}
+			>
+				<AlertDialogOverlay>
+					<AlertDialogContent>
+						<AlertDialogHeader fontSize='lg' fontWeight='bold'>
+							Log out
+						</AlertDialogHeader>
+
+						<AlertDialogBody>
+							Are you sure you want to log out of your account?
+						</AlertDialogBody>
+
+						<AlertDialogFooter>
+							<Button ref={cancelRef} onClick={onClose}>
+								Cancel
+							</Button>
+							<Button colorScheme='red' onClick={() => logout()} ml={3}>
+								Logout
+							</Button>
+						</AlertDialogFooter>
+					</AlertDialogContent>
+				</AlertDialogOverlay>
+			</AlertDialog>
+		</>
 	)
 }
 
