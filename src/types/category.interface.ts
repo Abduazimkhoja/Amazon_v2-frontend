8bd90daec3a5d1cacf87c6ab4752b0bc794041c5
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-05-22 17:54:55 +0500

    refactor: create interface ICategoryProduct

--- src/types/category.interface.ts
+++ src/types/category.interface.ts
@@ -1,5 +1,12 @@
+import { IProduct } from './product.interface'
+
 export interface ICategory {
 	id: number
 	name: string
 	slug: string
+}
+
+export interface ICategoryProduct {
+	title: string
+	products: IProduct[]
 }
