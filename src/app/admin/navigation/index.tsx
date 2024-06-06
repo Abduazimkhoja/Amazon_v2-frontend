0413f97e56a718a4c864bd62481fbc40bc38963a
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-06-06 08:57:24 +0500

    refactor: changed Link chakra ui to next Link

--- src/app/admin/navigation/index.tsx
+++ src/app/admin/navigation/index.tsx
@@ -1,26 +1,28 @@
 'use client'
 import { ADMIN_MENU } from '@/app/layout/sidebar/admin-menu.data'
-import { Box, Flex, Link } from '@chakra-ui/react'
+import { Box, Flex } from '@chakra-ui/react'
 import cn from 'clsx'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { FC } from 'react'
 
 const AuthNav: FC = () => {
 	const pathname = usePathname()
+
 	return (
 		<section className='container'>
 			<nav>
 				<Flex gap={10}>
-					{ADMIN_MENU.map(item => (
-						<Box key={item.href}>
+					{ADMIN_MENU.map(authNav => (
+						<Box key={authNav.href}>
 							<Link
-								href={item.href}
-								textDecoration='none'
-								_hover={{ borderBottom: '2px solid #000' }}
-								borderBottom={pathname === item.href ? '2px solid #000' : ''}
-								paddingBlock='1'
+								href={authNav.href}
+								className={cn(
+									pathname === authNav.href ? 'border-b-black' : '',
+									`no-underline py-2.5 border-b-2 border-transparent hover:border-black`
+								)}
 							>
-								{item.label}
+								{authNav.label}
 							</Link>
 						</Box>
 					))}
