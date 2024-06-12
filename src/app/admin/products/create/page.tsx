011fb5f8ce494edebeedb608a3c8f722ada3e7c8
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-06-12 21:55:41 +0500

    feat: add a request to get all categories in the create product page

--- src/app/admin/products/create/page.tsx
+++ src/app/admin/products/create/page.tsx
@@ -1,7 +1,14 @@
 import { FC } from 'react'
+import CreateProduct from './CreateProduct'
+import { CategoryService } from '@/services/category.service'
 
-const Page: FC = () => {
-    return <div>page</div>
+const getAllCategories = async () => {
+	return await CategoryService.getAll()
+}
+
+const Page: FC = async () => {
+	const { data: categories } = await getAllCategories()
+	return <CreateProduct categories={categories}/>
 }
 
 export default Page
